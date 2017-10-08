b0a9d2ee32bc408589bc74ed977a1c2df9542a80
FallDown <n/a>
2017-10-08 19:56:24 +0300

    Add basic info layout
    with dependencies for material and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Import the NgModule for each component you want to use:
+import { MatButtonModule, MatCheckboxModule, MatListModule } from '@angular/material';
 
+// Components
 import { AppComponent } from './app.component';
 
+// Services
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatButtonModule, MatCheckboxModule, MatListModule // these are just examples
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
