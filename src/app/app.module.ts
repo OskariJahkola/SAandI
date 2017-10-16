fac726e30b49b4780091f2cc74cbc699e1fca945
Kukkaherra <hevi-letti@hotmail.com>
2017-10-16 18:35:19 +0300

    Info page as accordion
    Now including: Very Janky Fix™

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ReactiveFormsModule } from '@angular/forms';
 // Import the NgModule for each component you want to use:
 import { 	MatButtonModule, MatCheckboxModule, MatListModule, MatToolbarModule,
-					MatTabsModule, MatIconModule, MatInputModule, MatSnackBarModule } from '@angular/material';
+					MatTabsModule, MatIconModule, MatInputModule, MatSnackBarModule,
+					MatExpansionModule } from '@angular/material';
 // Components
 import { AppComponent } from './app.component';
 import { InfoComponent } from './info/info.component';
@@ -16,7 +18,6 @@ import { FrontPageComponent } from './front-page/front-page.component';
 import { GmapComponent} from './gmap/gmap.component';
 import { GeolocationService } from './services/geolocation.service';
 import { environment } from '../environments/environment';
-import { ReactiveFormsModule } from '@angular/forms';
 
 // Services
 
@@ -42,6 +43,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 		MatIconModule,
 		MatInputModule,
 		MatSnackBarModule,
+		MatExpansionModule,
 		ReactiveFormsModule
 	],
 	providers: [GeolocationService],
--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'app-info',
 	templateUrl: './info.component.html',
-	styleUrls: ['./info.component.scss']
+	styleUrls: ['./info.component.scss'],
+	// Part of " Very Janky Fix™  "
+	encapsulation: ViewEncapsulation.None
 })
 export class InfoComponent implements OnInit {
 
